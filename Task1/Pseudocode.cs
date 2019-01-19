2a2166596fa7dea1257a9cce7e42eb03ee843985
Dipendra Humagain <dhumagai@gmail.com>
2019-01-19 11:59:15 -0700

    final

---
+++ Task1/Pseudocode.cs
@@ -0,0 +1,17 @@
+//setup
+double miles = 0.0;
+double gallon = 0.0;
+
+//input
+input "Enter the miles travelled by car."
+get miles;
+
+input "Enter the gallon of gas used."
+get gallon;
+
+//proccess
+fuelEfficiency = miles/gallon
+
+//output
+
+"Fuel effieciency for your car is " + fuelEfficiency + " mpg."
--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -6,7 +6,26 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //setup
+            double miles = 0.0;
+            double gallon = 0.0;
+            double fuelEfficiency = 0.0;
+
+            //input
+            Console.WriteLine("Enter the miles travelled by car");
+            miles = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the gallon of gas used.");
+            gallon = double.Parse(Console.ReadLine());
+
+            //process
+            fuelEfficiency = miles / gallon;
+
+            //output
+            Console.WriteLine("Fuel effieciency for your car is " + fuelEfficiency + " mpg.");
+
+            Console.ReadLine();
+           
         }
     }
 }
